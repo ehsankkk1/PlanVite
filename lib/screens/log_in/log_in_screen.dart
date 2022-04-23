99a4b7c1667daa06c1ab003b3ef792998f1eafe1
Ehsan Abourshaid <ehsan.ab101@gmail.com>
2022-04-23 21:22:13 +0300

    update

---
+++ lib/screens/log_in/log_in_screen.dart
@@ -0,0 +1,235 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:plane_vite/widgets/custom_text_field.dart';
+import '../../constants.dart';
+import 'log_in_controller.dart';
+
+class LoginScreen extends StatelessWidget {
+  //final GlobalKey<FormState> _globalKey = GlobalKey<FormState>();
+  LoginController controller = Get.find();
+
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      backgroundColor: kBackGround,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              const Align(
+                  alignment: Alignment(1, 0),
+                  child: Image(
+                    image: AssetImage('images/log_in_flower.png'),
+                  )),
+              Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(
+                        left: width * 0.19,
+                        right: controller.selectedLangBool.value
+                            ? width * 0.19
+                            : width * 0.34,
+                        top: height * 0.09,
+                        bottom: height * 0.02),
+                    child: Row(
+                      children: [
+                        Directionality(
+                          textDirection: TextDirection.ltr,
+                          child: Text(
+                            'Welcome Back'.tr,
+                            style: const TextStyle(
+                              fontSize: 35,
+                              color: kWritings,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                        left: width * 0.14,
+                        right: controller.selectedLangBool.value
+                            ? width * 0.14
+                            : width * 0.22,
+                        top: 0,
+                        bottom: 0),
+                    child: Row(
+                      children: [
+                        Text(
+                          'please sign in to your account'.tr,
+                          style: const TextStyle(
+                            fontSize: 20,
+                            color: kWritings,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                    left: width * 0.1,
+                    right: width * 0.1,
+                    top: height * 0.1,
+                    bottom: 0),
+                child: Directionality(
+                  textDirection: TextDirection.ltr,
+                  child: CustomTextField(
+                    fontSize: 15,
+                    hintText: 'Email',
+                    icon: Icons.email,
+                    textInputType: TextInputType.emailAddress,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                    left: width * 0.1,
+                    right: width * 0.1,
+                    top: height * 0.03,
+                    bottom: 0),
+                child: Directionality(
+                  textDirection: TextDirection.ltr,
+                  child: CustomTextField(
+                    hintText: 'Password',
+                    icon: Icons.lock,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                    left: width * 0.34,
+                    right: width * 0.34,
+                    top: height * 0.04,
+                    bottom: 0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: kTextFieldBorder,
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Login'.tr,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                      )),
+                ),
+              ),
+              Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(
+                        left: width * 0.25,
+                        right: controller.selectedLangBool.value
+                            ? width * 0.25
+                            : width * 0.35,
+                        top: height * 0.02,
+                        bottom: 0),
+                    child: Row(
+                      children: [
+                        Text(
+                          'Don\'t have an account?'.tr,
+                          style: const TextStyle(
+                            color: kGrey,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Get.offNamed('/signup');
+                    },
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                          left: controller.selectedLangBool.value
+                              ? width * 0.42
+                              : width * 0.01,
+                          right: controller.selectedLangBool.value
+                              ? width * 0.01
+                              : width * 0.38,
+                          top: height * 0.003,
+                          bottom: 0),
+                      child: Row(
+                        children: [
+                          Text(
+                            'Sign up'.tr,
+                            style: const TextStyle(
+                              color: kTextFieldBorder,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                        left: width * 0.33,
+                        right: width * 0.02,
+                        top: height * 0.04,
+                        bottom: 0),
+                    child: Directionality(
+                      textDirection: TextDirection.ltr,
+                      child: Row(
+                        children: [
+                          GestureDetector(
+                            child: TextButton(
+                                onPressed: () {
+                                  controller.selectedLang = 'en';
+                                  controller.changeSelectedLang();
+
+                                  Get.updateLocale(
+                                      Locale(controller.selectedLang));
+                                },
+                                child: Text(
+                                  'English',
+                                  style: TextStyle(
+                                    color: controller.selectedLangBool.value
+                                        ? kTextFieldBorder
+                                        : kGrey,
+                                    fontSize: 18,
+                                  ),
+                                )),
+                          ),
+                          GestureDetector(
+                            child: TextButton(
+                                onPressed: () {
+                                  controller.selectedLang = 'ar';
+                                  controller.changeSelectedLang();
+
+                                  Get.updateLocale(
+                                      Locale(controller.selectedLang));
+                                },
+                                child: Text(
+                                  'العربية',
+                                  style: TextStyle(
+                                    color: controller.selectedLangBool.value
+                                        ? kGrey
+                                        : kTextFieldBorder,
+                                    fontSize: 18,
+                                  ),
+                                )),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utils/en.dart
@@ -0,0 +1,9 @@
+Map <String, String> en={
+  'Email': 'Email',
+  'Password': 'Password',
+  'Login':'Login',
+  'Welcome Back':'Welcome Back',
+  'please sign in to your account':'please sign in to your account',
+  'Don\'t have an account?':'Don\'t have an account?',
+  'Sign up':'Sign up',
+};
