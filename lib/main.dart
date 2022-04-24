6c17d136ebc5031a74c53927f9f250b79133dcfe
Ehsan Abourshaid <ehsan.ab101@gmail.com>
2022-04-24 21:59:30 +0300

    Welcome screen refactor

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
       translations:  Translation(),
       locale: Locale('en'),
       fallbackLocale: Locale('en'),
-      initialRoute: '/home',
+      initialRoute: '/welcome',
 
       getPages: [
 
--- lib/screens/welcome/welcome_screen.dart
+++ lib/screens/welcome/welcome_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:plane_vite/constants.dart';
@@ -12,75 +13,60 @@ class WelcomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: kBackGround,
       body: SafeArea(
-
         child: Column(
-         children: [
-           Padding(
-  padding: EdgeInsets.only(left: width*0.1,right: width*0.1,top: height*0.12,bottom: 0),
-  child:   Text('Task Management',
-
-  style: TextStyle(
-
-    color: kWritings,
-
-    fontSize: 46,
-
-  ),
-
-
-
-  ),
-),
-           Padding(
-             padding: EdgeInsets.only(left: width*0.1,right: width*0.12,top: 0,bottom: 0),
-             child:   Text('Manage your daily task easier like never before. Easy to use',
-
-               style: TextStyle(
-
-                 color: kWritings,
-
-                 fontSize: 20,
-
-               ),
-
-
-
-             ),
-           ),
-
-           Padding(
-             padding: EdgeInsets.only(left: width*0.1,right:width *0.4,top: height*0.04,bottom: 0),
-             child: GestureDetector(
-               onTap: (){
-                 Get.offNamed('/login');
-               },
-               child: Container(
-                 width:
-                      width * 0.5,
-
-                 decoration: BoxDecoration(
-                   color: kTextFieldBorder,
-                   borderRadius: BorderRadius.circular(25.0),
-                 ),
-                 child: TextButton(
-                     onPressed: () { Get.toNamed('/login');},
-                     child: Text(
-                       'Let’s Get Started',
-                       style: const TextStyle(
-                         color: Color(0xFFFCF8F3),
-                         fontSize: 20,
-                       ),
-                     )),
-               ),
-             ),
-           ),
-
-           Padding(
-             padding: EdgeInsets.only(left: 0,right:0,top: height*0.11,bottom: 0),
-             child: Image(image: AssetImage('images/welcome_pic.png'),),
-           )
-
-         ],
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: EdgeInsets.fromLTRB(width * 0.1,height * 0.12,width * 0.1,0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Task Management',
+                    style: TextStyle(
+                      color: kWritings,
+                      fontSize: 46,
+                    ),
+                  ),
+                  const SizedBox(height: 20,),
+                  const Text(
+                    'Manage your daily task easier like never before. Easy to use',
+                    style: TextStyle(
+                      color: kWritings,
+                      fontSize: 20,
+                    ),
+                  ),
+                  const SizedBox(height: 20,),
+                  GestureDetector(
+                    onTap: () {
+                      Get.offNamed('/login');
+                    },
+                    child: Container(
+                      width: width * 0.5,
+                      decoration: BoxDecoration(
+                        color: kTextFieldBorder,
+                        borderRadius: BorderRadius.circular(25.0),
+                      ),
+                      child: TextButton(
+                          onPressed: () {
+                            Get.toNamed('/login');
+                          },
+                          child: const Text(
+                            'Let’s Get Started',
+                            style: TextStyle(
+                              color: Color(0xFFFCF8F3),
+                              fontSize: 20,
+                            ),
+                          )),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Image(
+              image: AssetImage('images/welcome_pic.png'),
+            )
+          ],
         ),
       ),
     );
