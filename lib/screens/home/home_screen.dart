39922454dba7c679a2c4e82a143a7dadcc016edf
Ehsan Abourshaid <ehsan.ab101@gmail.com>
2022-04-24 19:03:56 +0300

    add color

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,34 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_zoom_drawer/config.dart';
 import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
 import 'package:get/get.dart';
+import 'package:plane_vite/constants.dart';
 import '../Drawer/drawer_controller.dart';
 import '../Drawer/drawer_screen.dart';
-
 
 
 class HomeScreen extends StatelessWidget {
   @override
   MyDrawerController controller = Get.find();
   Widget build(BuildContext context) {
-    return GetBuilder<MyDrawerController>(
-      builder: (_) => ZoomDrawer(
-
-        style: DrawerStyle.defaultStyle,
-        controller: _.zoomDrawerController,
-        menuScreen: DrawerScreen(),
-        mainScreen: MainScreen(),
-        borderRadius: 24.0,
-        showShadow: true,
-        angle: -4,
-        drawerShadowsBackgroundColor: Colors.grey,
-        slideWidth: MediaQuery.of(context).size.width * 0.65,
+    return Scaffold(
+      backgroundColor: kBackGround,
+      body: GetBuilder<MyDrawerController>(
+        builder: (_) => ZoomDrawer(
+          moveMenuScreen: true,
+          controller: _.zoomDrawerController,
+          menuScreen: DrawerScreen(),
+          mainScreen: _MainScreen(),
+          borderRadius: 10.0,
+          showShadow: true,
+          angle: 0,
+          slideWidth: MediaQuery.of(context).size.width * 0.75,
+        ),
       ),
     );
   }
 }
 
-class MainScreen extends StatelessWidget {
+class _MainScreen extends StatelessWidget {
   MyDrawerController controller = Get.find();
   @override
   Widget build(BuildContext context) {
