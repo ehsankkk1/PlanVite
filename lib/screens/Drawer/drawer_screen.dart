b58a8de28360c771f19a55ad8ee62dc95feb0dcf
Ehsan Abourshaid <ehsan.ab101@gmail.com>
2022-04-29 00:37:56 +0300

    Drawer 1st

--- lib/screens/Drawer/drawer_screen.dart
+++ lib/screens/Drawer/drawer_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get_state_manager/src/simple/get_view.dart';
+import 'package:plane_vite/widgets/drawer_item.dart';
 import '../../constants.dart';
 import 'drawer_controller.dart';
 
@@ -9,9 +11,71 @@ class DrawerScreen extends GetView<MyDrawerController> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-        color: kLightPink,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: NotificationListener<OverscrollIndicatorNotification>(
+          onNotification: (overScroll) {
+            overScroll.disallowGlow();
+            return true;
+          },
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children:  [
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children:const [
+                       CircleAvatar(radius:35,backgroundColor: kWritings,child: Text('E',style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),),
+                       SizedBox(height: 25,),
+                       Text(
+                        'Ehsan Abourshaed',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: kWritings,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                        SizedBox(height: 10,),
+                       Text(
+                        '+963945057206',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: kWritings,
+
+                        ),
+                      ),],
+                  ),
+                ),
+
+                DrawerItem(Icons.home,35, 'Home', (){}),
+                const Divider(thickness:1,color: kMainPink,),
+                const Padding(
+                  padding: EdgeInsets.all(15.0),
+                  child: Text(
+                    'WorkSpaces',
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.black54,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Column(
+                  children: List.generate(
+                    3, (index) => DrawerItem(FontAwesomeIcons.userGroup,25, 'Sample Project', (){print(index);}),),
+                ),
+                DrawerItem(Icons.add,35, 'Add Project', (){}),
+                const Divider(thickness:1,color: kMainPink,),
+                DrawerItem(Icons.task,35, 'Todo Tasks', (){}),
+                DrawerItem(Icons.notifications,35, 'Notifications', (){}),
+                DrawerItem(Icons.settings,35, 'Settings', (){}),
+            ],
+    ),
+          ),
+        ),
       ),
     );
   }
---
+++ lib/widgets/drawer_item.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../constants.dart';
+
+
+class DrawerItem extends StatelessWidget {
+   DrawerItem(
+       this.icon,
+       this.size,
+       this.name,
+       this.onPress,
+       );
+
+  IconData icon;
+  double size;
+  String name;
+  VoidCallback onPress;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(15,15,0,15),
+        child: Row(
+          children:  [
+            Icon(icon,color: kMainPink,size: size,),
+            const SizedBox(width: 25,),
+            Text(
+              name,
+              style: const TextStyle(
+                fontSize: 15,
+                color: kWritings,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+          ],
+        ),
+      ),
+      onTap: onPress,
+    );
+  }
+}
