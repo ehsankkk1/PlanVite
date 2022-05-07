90d8fd6c6772a05f306b3ae3680956b2eb80c200
johny koudsi <johnykodsy@gmail.com>
2022-05-07 01:57:29 -0700

    splash commit

--- lib/config/server_config.dart
+++ lib/config/server_config.dart
@@ -1,5 +1,5 @@
 class ServerConfig {
-  static const domainNameServer='http://192.168.1.114:8000/';
+  static const domainNameServer='http://192.168.1.100:8000/';
   static  const checkValid='api/auth/check_valid';
 
 
