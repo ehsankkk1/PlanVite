e87533b7d3c65dcfbeb114c5ac55309c0ad32674
Ehsan Abourshaed <ehsan.ab101@gmail.com>
2022-07-04 00:35:53 +0300

    added responsive text to Notifications

--- lib/screens/notifications/notifications_screen.dart
+++ lib/screens/notifications/notifications_screen.dart
@@ -1,13 +1,17 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:plane_vite/constants.dart';
 import 'package:plane_vite/screens/notifications/notifications_controller.dart';
+
+import '../../config/SizeConfig.dart';
 
 
 class NotificationsScreen extends StatelessWidget {
   NotificationsController controller = Get.find();
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     final String locale = Get.locale.toString();
 
     return Directionality(
@@ -28,60 +32,46 @@ class NotificationsScreen extends StatelessWidget {
               ),
             ),
           ),
-          body: GridView.builder(
+          body: ListView.builder(
               itemCount: 30,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 1,
-                childAspectRatio: 4,
-              ),
-              itemBuilder: (context, index) => Padding(
-                    padding: EdgeInsets.only(
-                        left: width * 0.05,
-                        right: width * 0.05,
-                        bottom: height * 0.001,
-                        top: height * 0.001),
-                    child: Container(
-                      color: kBackGround,
-                      child: Column(
-                        children: [
-                          Row(
-                            children: [
-                              SizedBox(
-                                width: width * 0.02,
-                              ),
-                              const CircleAvatar(
-                                radius: 37,
-                                backgroundColor: kMainPink,
-                                child: CircleAvatar(
-                                  radius: 35,
-                                  backgroundColor: Colors.black12,
-                                  foregroundImage: AssetImage('images/joey.png'),
-                                ),
-                              ),
-                              SizedBox(
-                                width: width * 0.02,
-                              ),
-                              const Flexible(
-                                child: Text(
-                                  'Ehsan abourashed mentioned you in a comment',
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                    color: kWritings,
-                                  ),
-                                ),
-                              )
-                            ],
+              itemBuilder: (context, index) => Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: Row(
+                      children: [
+                        const CircleAvatar(
+                          radius: 37,
+                          backgroundColor: kMainPink,
+                          child: CircleAvatar(
+                            radius: 35,
+                            backgroundColor: Colors.black12,
+                            foregroundImage: AssetImage('images/joey.png'),
                           ),
-                          Divider(
-                            height: height * 0.02,
-                            endIndent: 1,
-                            thickness: 1.5,
-                            color: kMainPink,
-                          )
-                        ],
-                      ),
+                        ),
+                        SizedBox(
+                          width: width * 0.02,
+                        ),
+                         Flexible(
+                          child: Text(
+                            'Ehsan abourashed mentioned you in a comment',
+                            style: TextStyle(
+                              fontSize: 4.5*SizeConfig.blockSizeHorizontal,
+                              color: kWritings,
+                            ),
+                          ),
+                        )
+                      ],
                     ),
-                  ))),
+                  ),
+                  Divider(
+                    height: height * 0.01,
+                    endIndent: 1,
+                    thickness: 1,
+                    color: kMainPink,
+                  )
+                ],
+              ))),
     );
   }
 }
